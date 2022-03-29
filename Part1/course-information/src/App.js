d51f8295bb7c05bad482b52699af578b2356914a
Jeremiah Martinez <itsjeremiahmartinez@gmail.com>
2022-03-29 20:26:41 +1300

    Refactored Content component into Parts
    Content component removed, now using Part components to make each exercise name and part an individual component.

--- Part1/course-information/src/App.js
+++ Part1/course-information/src/App.js
@@ -6,42 +6,35 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      {props.part.name} {props.part.exercise}
     </div>
   )
 }
 
 const Total = (props) => {
   return (
-    <p>Number of exercsies {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <div>
+      <p>Number of exercises {props.part1.exercise + props.part2.exercise + props.part3.exercise}</p>
+    </div>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {name: 'Fundamentals of React', exercise: 10}
+  const part2 = {name: 'Using props to pass data', exercise: 7}
+  const part3 = {name: 'State of a component', exercise: 14}
 
   return (
     <>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Part part={part1}/>
+      <Part part={part2}/>
+      <Part part={part3}/>
+      <Total part1={part1} part2={part2} part3={part3}/>
     </>
   )
 }
