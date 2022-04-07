70f315743b6342ec87ab79e38278d73f854e14b0
Jeremiah Martinez <itsjeremiahmartinez@gmail.com>
2022-04-07 19:07:45 +1200

    Application now supports multiple courses
    Ex 2.4 & 2.5 completed
    
    Application works for multiple courses in a curriculum. Curriculum component created and Course component already existing.

--- Part2/course-information/src/App.js
+++ Part2/course-information/src/App.js
@@ -1,11 +1,13 @@
-const Header = ({ course }) => {
+//Header component
+const Header = ({ title }) => {
   return (
     <div>
-      <h1>{course}</h1>
+      <h3>{title}</h3>
     </div>
   )
 }
 
+//Part component
 const Part = ({ part }) => {
   return (
     <div>
@@ -14,6 +16,7 @@ const Part = ({ part }) => {
   )
 }
 
+//Content component
 const Content = ({ parts }) => {
   const partComponents = parts.map(part => <Part part={{name: part.name, exercise: part.exercises}}/>)
 
@@ -24,50 +27,92 @@ const Content = ({ parts }) => {
   )
 }
 
+//Total component
 const Total = ({ parts }) => {
   var total = parts.reduce((count, part) => count + part.exercises, 0)
 
   return (
     <div>
-      <p>Number of exercises {total}</p>
+      <p><b>Number of exercises {total}</b></p>
     </div>
   )
 }
 
+//Course component
 const Course = ({ course }) => {
   return (
     <div>
-      <Header course={course.name}/>
+      <Header title={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts}/>
     </div>
   )
 }
 
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+//Curriculum component
+const Curriculum = ({ courses }) => {
+  const courseComponents = courses.map(course => <Course course={course}/>)
 
-  return <Course course={course} />
+  return (
+    <div>
+      {courseComponents}
+    </div>
+  )
+}
+
+//App component
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <>
+      <Header title='Web development curriculum' />
+      <Curriculum courses={courses} />
+    </>
+  )
 }
 
 export default App
