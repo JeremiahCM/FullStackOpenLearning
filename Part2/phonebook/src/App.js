f5eeba046d20c6bd96265e6ba518f2e6568c1a21
Jeremiah Martinez <itsjeremiahmartinez@gmail.com>
2022-04-14 17:24:24 +1200

    Phonebook app initialized. Add new name functionality
    Exercise 2.6 completed
    
    Phonebook app initialized. Ability to add new person to list by name.

---
+++ Part2/phonebook/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const Person = ({ name }) => {
+  return (
+    <div>
+      {name}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <Person key={person.name} name={person.name} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
