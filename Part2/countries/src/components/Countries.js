818dddc574423b8e69c8463e368e59e1a9797fa1
Jeremiah Martinez <itsjeremiahmartinez@gmail.com>
2022-05-01 19:32:22 +1200

    Can show basic data for countries that appear in filter search
    Exercise 2.13 completed.
    
    Countries shown in filter search now have 'show' buttons. When clicked, it will show the basic data for that country.

--- Part2/countries/src/components/Countries.js
+++ Part2/countries/src/components/Countries.js
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import Country from './Country'
 
 const Countries = ({ countries }) => {
+    const [showDetailed, setShowDetailed] = useState('')
+
     const tooManyCountries = countries.length > 10 ? true : false
     const enoughCountries = countries.length > 1 && countries.length <= 10 ? true : false
     const exactCountry = countries.length === 1 ? true : false
@@ -11,8 +14,13 @@ const Countries = ({ countries }) => {
         {enoughCountries && 
           <ul>
             {countries.map(country =>
-            <div key={country.name.common}>{country.name.common}</div>)}
+            <div key={country.name.common}>
+                {country.name.common} <button onClick={() => setShowDetailed(country)}>show</button>
+            </div>)}
           </ul>
+        }
+        {showDetailed !== '' &&
+            <Country country={showDetailed}/>
         }
         {exactCountry && <Country country={countries[0]}/> }
       </div>
